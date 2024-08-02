de6abcc1ec29a190d155b226cd59c82105e2831e
Anastasiya <vipstop2303@mail.ru>
2024-08-02 10:54:07 +0300

    Контрольная итоговая работа

--- Контрольная итоговая работа.py
+++ Контрольная итоговая работа.py
@@ -11,20 +11,13 @@ data = pd.DataFrame({'whoAmI'lst})
 data.head() |
 """
 
-import pandas as pd 
-import numpy as np 
 import random
- 
+import pandas as pd
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI': lst})
-print(data)
- 
-#==================================================#
-data['tmp'] = 1
-data.set_index([data.index, 'whoAmI'], inplace=True)
-data = data.unstack(level=-1, fill_value = 0).astype(int)
-data.columns = data.columns.droplevel()
-data.columns.name = None
-print(data)
+data = pd.DataFrame({'whoAmI':lst})
+data.loc[data['whoAmI'] == 'robot', 'robot' ] = '1'
+data.loc[data['whoAmI'] == 'human', 'human' ] = '1'
+data = data.fillna(0)
+data.head(20)
